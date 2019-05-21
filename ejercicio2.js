2095ae84e49135f406d84ba9489f2e7366d7b3db
UranioRial <50871460+LegalizaWeed@users.noreply.github.com>
2019-05-21 09:21:42 -0300

    Update ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,24 @@
+var name="Uranio"
+
+var age="15"
+
+var ismember="SI"
+
+var gender="hombre"
+
+var thirsty="yes"
+
+var drinkName="CocaCola"
+
+var person=prompt("hola como te llamas",name)
+
+var person=prompt("cuantos años tenes",age)
+
+var person=prompt("Sos miembro",ismember)
+
+var person=prompt("Sos hombre o mujer",gender)
+
+var person=prompt("tenes sed", thirsty)
+
+var person=prompt("que queres tomar", drinkName)
+
